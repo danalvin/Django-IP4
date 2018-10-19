f8fd3c1df9887d4d6dfccb6eaac9e7fc1892434d
danalvin <mungaiwaituika@gmail.com>
2018-10-19 18:10:50 +0300

    settings and urls

--- hoodie/views.py
+++ hoodie/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
 
+@login_required(login_url='/accounts/login/')
 def home(request):
     return render(request, 'index.html')
 
 
+@login_required(login_url='/accounts/login/')
 def loader(request):
     return render (request, 'loader.html')
