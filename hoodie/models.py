48cdb754f1f6a5be03d5c49872ed688bad282039
danalvin <mungaiwaituika@gmail.com>
2018-10-19 17:30:53 +0300

    models adjucted

--- hoodie/models.py
+++ hoodie/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 
 
 class nieghbor(models.Model):
-    name = charfield(max_length=100, blank=True, null=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="hoodadmin", null=True, blank=True)
     location = models.CharField(max_length=100, null=True, blank=True)
 
@@ -14,6 +15,9 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     ID = models.CharField(max_length=100, null=True, blank=True)
     profilepicture = models.ImageField(upload_to='images/', blank=True, default="/black.png")
-    neighbor_hood = models.ForeignKey(Neighbor_hood, on_delete=models.CASCADE, related_name="hood", null=True,
+    neighbor_hood = models.ForeignKey(nieghbor, on_delete=models.CASCADE, related_name="hood", null=True,
                                       blank=True)
     secondary_email = models.CharField(max_length=100, null=True, blank=True)
+
+
+
