1709ee03fda28da683e89c8f097a83094e363b54
danalvin <mungaiwaituika@gmail.com>
2018-10-19 15:59:57 +0300

    models added

--- hoodie/models.py
+++ hoodie/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class nieghbor(models.Model):
+    name = charfield(max_length=100, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="hoodadmin", null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    ID = models.CharField(max_length=100, null=True, blank=True)
+    profilepicture = models.ImageField(upload_to='images/', blank=True, default="/black.png")
+    neighbor_hood = models.ForeignKey(Neighbor_hood, on_delete=models.CASCADE, related_name="hood", null=True,
+                                      blank=True)
+    secondary_email = models.CharField(max_length=100, null=True, blank=True)
